ca49418f6e4b5a3fedd2d10631c4d0ca562359c5
Michael <michael.j.lukiman@gmail.com>
2019-10-01 23:40:31 -0400

    Add searchMap add-remove

--- MLL469_contact-book/src/ContactBookTool.java
+++ MLL469_contact-book/src/ContactBookTool.java
@@ -1,31 +1,87 @@
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
 
 import org.json.JSONObject;
 
 public class ContactBookTool {
 		
-	private HashMap<String, HashMap<String, ContactCard>> searchMap = new HashMap<String, HashMap<String, ContactCard>>();
+	// Internal structure as follows: index into the map through an existing value ('Bob', '311', 'example@example.com').
+	// O(1) time, worst-case O(cards * fields) space
+	// Index into the subsequent map with the keys it has values for ('name', 'number', 'email' respectively).
+	// O(1) time
+	// What is returned is a list of ContactCards which matches those values for those fields (Mostly one item lists, but can be many).
+	// For example if two contacts are named 'Bob', the returned List will be the two ContactCards of the 'Bob's, which are distinct. 
+	private HashMap<String, HashMap<String, List<ContactCard>>> searchByValueMap = new HashMap<String, HashMap<String, List<ContactCard>>>();
 	
 	public ContactBookTool() {
 		
 	}
 	
-	public HashMap<String, Integer> searchFor(String field, String value) {
-		HashMap<String, Integer> results = searchMap.get(field);
+	public HashMap<String, List<ContactCard>> searchForValue(String value) {
+		HashMap<String, List<ContactCard>> results = searchByValueMap.get(value);
 		return results;
 	}
 	
-	public void addContact(JSONObject updatedInfo){
-		ContactCard contact = ContactCard.createEmptyCard(); 
-		contact.dictionaryToFieldUpdate(updatedInfo);
-		
-		Iterator<?> keys = updatedInfo.keys();
-		int hashCode = contact.hashCode();
-		while (keys.hasNext() ) {
-			String key = (String)keys.next();
-			String value = updatedInfo.getString(key);
-			searchMap.put(key, )
+	public  List<ContactCard> searchValuesForMatchingField(String field, HashMap<String, List<ContactCard>> resultsOfSearchForVal) {
+		if (resultsOfSearchForVal.containsKey(field)){
+			List<ContactCard> results = resultsOfSearchForVal.get(field);
+			return results;
 		}
+		else { 
+			return null;
+		}
+	}
+	
+	public void addContact(JSONObject cardInfo){
+		ContactCard immutableCard = ContactCard.immutableCard(cardInfo);
+		
+	}
+	
+	private void addToSearchMap(ContactCard contact) {
+		HashMap<String, String> cardProps = contact.getProperties();
+		
+		for (Entry<String, String> entry : cardProps.entrySet()) {
+		
+			String key = entry.getKey();
+			String value = entry.getValue();
+			if (this.searchByValueMap.containsKey(value)) {
+				HashMap<String, List<ContactCard>> matchingFields = this.searchByValueMap.get(value);
+				matchingFields.add(contact)
+			}
+			else {
+				
+			}
+		}
+	}
+	
+	
+	private void removeFromSearchMap(ContactCard contact) {
+		HashMap<String, String> cardProps = contact.getProperties();
+		
+		for (Entry<String, String> entry : cardProps.entrySet()) {
+		
+			String key = entry.getKey();
+			String value = entry.getValue();
+			
+			HashMap<String, List<ContactCard>> fieldMap = searchByValueMap.get(value);
+			
+			for (Entry<String, List<ContactCard>> cardRef : fieldMap.entrySet()) {
+				
+				String field = cardRef.getKey();
+				List<ContactCard> matchingCards = cardRef.getValue();
+				List<ContactCard> dummyMatchingCards = new ArrayList<ContactCard>(matchingCards); 
+				for (ContactCard card : matchingCards) {
+					if (card.hashCode() == contact.hashCode()) { 
+						matchingCards.remove(card);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	public void editContact(JSONObject updatedInfo, ContactCard contact){
 	}
 }
--- MLL469_contact-book/src/ContactCard.java
+++ MLL469_contact-book/src/ContactCard.java
@@ -8,7 +8,9 @@ import org.json.JSONObject;
  *
  */
 public class ContactCard {
-	HashMap<String, String> cardProperties;
+	private HashMap<String, String> cardProperties;
+	private int hashCodeVal = -1; 
+	
 	private ContactCard() {
 		cardProperties = new HashMap<String, String>();
 		cardProperties.put("name", "Empty");
@@ -17,18 +19,31 @@ public class ContactCard {
 		cardProperties.put("address","No address");
 	};
 	
-	public static ContactCard createEmptyCard() {
+	public static ContactCard immutableCard(JSONObject jObject) {
+		ContactCard contact = createEmptyCard();
+		contact.dictionaryToFieldUpdate(jObject);
+		return contact;
+	}
+	
+	private static ContactCard createEmptyCard() {
 		ContactCard contact = new ContactCard(); 
 		return contact;
 	}
 	
-	public void dictionaryToFieldUpdate(JSONObject jObject) {
+	private void dictionaryToFieldUpdate(JSONObject jObject) {
 			Iterator<?> keys = jObject.keys();
 			
 			while (keys.hasNext() ) {
 				String key = (String)keys.next();
 				String value = jObject.getString(key);
-				this.cardProperties.put(key, value);
+				if (this.cardProperties.containsKey(key)) { // Prevents superfluous information in the JSON making it into the ContactCard data structure. 
+					this.cardProperties.put(key, value); 
+				}
+			}
+	}
+	
+	public HashMap<String, String> getProperties() {
+		return cardProperties;
 	}
 	
 	@Override
@@ -37,17 +52,22 @@ public class ContactCard {
 		 * Returns a unique code using all of object's fields, used to locate the object in hashmap.
 		 * @return An integer primitive to be used as the object's unique hashcode.
 		 */
-		String name = cardProperties.get("name");
-		String number = cardProperties.get("number");
-		String email = cardProperties.get("email");
-		String address = cardProperties.get("address");
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((number == null) ? 0 : number.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((email == null) ? 0 : address.hashCode());
-		return result;
+		if (this.hashCodeVal != -1) { 
+			return hashCodeVal;
+			} // Prevents from rehashing code if already exists. The public points of entry to class only allows new objects, so hashcode does not change. 
+		else {
+			String name = cardProperties.get("name");
+			String number = cardProperties.get("number");
+			String email = cardProperties.get("email");
+			String address = cardProperties.get("address");
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			result = prime * result + ((number == null) ? 0 : number.hashCode());
+			result = prime * result + ((email == null) ? 0 : email.hashCode());
+			result = prime * result + ((email == null) ? 0 : address.hashCode());
+			return result;
+		}
 	};
 	
 	
